c08de0293d2f2f76690c2e065ee044589f6c2a07
alamgirmani <alamgir.mani@gmail.com>
2021-06-09 14:33:49 +0200

    added a feature to send email #2

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from service import send_email
 
 server = Flask(__name__)
 
@@ -9,6 +10,11 @@ def hello():
     hello world
     :return:
     """
+    sender = "mani.ghga@gmail.com"
+    receiver = "manikandan.ravichandran@dkfz-heidelberg.de"
+    subject = "hey"
+    message = "hello world 3"
+    send_email.send_email(sender, receiver, subject, message)
     return "Hello World!"
 
 
--- src/service/send_email.py
+++ src/service/send_email.py
@@ -2,10 +2,23 @@
 Send email
 """
 
+import smtplib
+from email.message import EmailMessage
 
-def send_email():
+
+def send_email(sender, receiver, subject, message):
     """
     send email
     :return:
     """
-    print("send email")
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+    msg.set_content(message)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(sender, "")
+    server.send_message(msg)
+    server.quit()
