eca5866048982d221f361fdc95442eeb17d0c62f
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2021-06-30 17:36:35 +0200

    Set up the barebones system for RabbitMQ. Might need to do some file standarisation fixes, since I followed sandbox-storage, which has not been standardised yet.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,30 @@
-FROM tiangolo/uvicorn-gunicorn-fastapi:latest
+# Copyright 2021 Universität Tübingen, DKFZ and EMBL for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-WORKDIR /app
-
-COPY . /app
-
-RUN pip install -r requirements.txt
+FROM python:3.9-slim
 
 EXPOSE 8000
+EXPOSE 15672
 
-ENTRYPOINT ["python"]
+# copy and install package from source
+COPY . /app
+WORKDIR /app
+RUN pip install .
 
-CMD [ "./sandbox_notification/app.py" ]
+# create new user and execute as that user:
+RUN useradd --create-home appuser
+WORKDIR /home/appuser
+USER appuser
+
+ENTRYPOINT [ "sandbox-notification" ]
--- sandbox_notification/app.py
+++ sandbox_notification/app.py
@@ -2,35 +2,17 @@
 app.py This module serves as the entry point for the application.
 """
 
-import uvicorn
+#!/usr/bin/env python
+import pika
 
-from fastapi import FastAPI
-from service import send_mail
-from config import config
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+channel = connection.channel()
 
-app = FastAPI()
-CONFIG_YAML = "example_config.yaml"
-HOST = 'host'
-PORT = 'port'
-config_data = config.get_config(CONFIG_YAML)
+channel.queue_declare(queue='hello')
 
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
 
-@app.get("/")
-async def send_email():
-    """
-    send mail
-    :return:
-    """
-    sender = "example@gmail.com"
-    receiver = ["receiver@example.com"]
-    subject = "hey"
-    message = "hello world"
-    send_mail.send_email(sender, receiver, subject, message)
-    return "Mail sent"
-
-
-if __name__ == "__main__":
-    uvicorn.run("app:app",
-                host=config_data[HOST],
-                port=config_data[PORT],
-                log_level="info")
+connection.close()
