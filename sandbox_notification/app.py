21d1ace20320da64c26b985250d3ede6f09d87cc
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2021-07-01 20:38:52 +0200

    Added message consumption on the same app file, to be sseparated later if needed

--- sandbox_notification/app.py
+++ sandbox_notification/app.py
@@ -3,7 +3,7 @@ app.py This module serves as the entry point for the application.
 """
 
 #!/usr/bin/env python
-import pika
+import pika, sys, os
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 channel = connection.channel()
@@ -14,5 +14,22 @@ channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
 print(" [x] Sent 'Hello World!'")
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(queue='hello',
+                      auto_ack=True,
+                      on_message_callback=callback)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
 
 connection.close()
