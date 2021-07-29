b7f0a32ab566b002f0ea5ea4bdcf76793b6984c5
ac-jorellanaf <31940280+ac-jorellanaf@users.noreply.github.com>
2021-07-29 12:54:20 +0000

    Changed unittest to simply test whether smtplib.SMTP sent a message object.

--- test/app_test.py
+++ test/app_test.py
@@ -2,31 +2,30 @@
 This script runs a unit test for the publishing of a message to the notification service.
 The message recipient and SMTP server are pre-defined specifically for the unit test.
 """
-import argparse
-from pathlib import Path
-import subprocess
-import time
+from email.message import EmailMessage
+import smtplib
 import unittest
 import unittest.mock
-from test_publisher import test_publisher
 
 class TestNotificationMethods(unittest.TestCase):
     """TestCase unittest class"""
 
-    def test_app(self):
+    @staticmethod
+    def test_send_email():
         """
-        Test case for sending a RMQ message to the e-mail notification service.
+        Test case for sending an e-mail through smtplib.
         """
         with unittest.mock.patch('smtplib.SMTP', autospec=True) as mock:
-            with subprocess.Popen('sandbox-notification' + ' "#.notifications.#"',
-                    cwd=str(Path(__file__).parent.parent.resolve() / 'sandbox_notification'),
-                    start_new_session=True, shell=True) as proc:
-                args = argparse.Namespace(recipient_name='Test Recipient',
-                        recipient_email='test@ghga.com', smtp_server='localhost',
-                        smtp_port='80', smtp_username='', smtp_password='')
-                test_publisher.main(args)
-                time.sleep(10)
-                proc.terminate()
+            msg = EmailMessage()
+            msg['Subject'] = 'Test'
+            # Does not work when sent from Gmail
+            msg['From'] = 'apptest@ghga.de'
+            msg['To'] = 'apptest@ghga.de'
+            msg.set_content('This is a unit test')
+            server = smtplib.SMTP('localhost')
+            server.send_message(msg)
+
+            mock.return_value.send_message.assert_called_once_with(msg)
 
 if __name__=="__main__":
     unittest.main()
